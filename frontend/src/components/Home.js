c92da73180491a8ca24fe6c74b994a14742958b2
EricLipe17 <ericlipe17@gmail.com>
2025-01-01 16:51:36 -0500

    - name viper

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h3>SPA App - Home</h3>
+        <h3>V.I.P.E.R - Home</h3>
         <p>This is a paragraph on the HomePage of the SPA App.</p>
       </div>
     );
