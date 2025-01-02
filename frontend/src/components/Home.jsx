a9b799f672191e352be1145fec107e4e2e623053
EricLipe17 <ericlipe17@gmail.com>
2025-01-02 16:24:01 -0500

    - Fixing the border because it was annoying

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <div className="bg-black-950 min-h-screen flex flex-col items-center justify-center text-white">
       <h1 className="text-5xl font-bold text-gold mb-4">V.I.P.E.R - Home</h1>
-      <p className="text-lg text-gray-300 mb-8">
+      <p className="text-lg mb-8">
         This is a paragraph on the HomePage of the SPA App.
       </p>
-      <button className="bg-gold text-black py-2 px-4 rounded hover:bg-white hover:text-black transition duration-300">
+      <button className="py-2 px-4 rounded border border-gold bg-black hover:bg-gold transition duration-300">
         Learn More
       </button>
     </div>
