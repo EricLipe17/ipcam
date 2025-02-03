effee02d57d4fbb54055ae8a751625df136cd077
EricLipe17 <ericlipe17@gmail.com>
2025-02-02 23:49:15 -0500

    - adding workers argument even though it doesnt work with hot reload

--- backend/app/main.py
+++ backend/app/main.py
@@ -81,4 +81,6 @@ app.include_router(users.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, lifespan="on")
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=8000, reload=True, lifespan="on", workers=5
+    )
